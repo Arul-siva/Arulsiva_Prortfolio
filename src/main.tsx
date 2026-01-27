307104cfae4aabbafce5f69519c1f7b6c9c8a1ab
Arul Shiva <jayabalarulsjiva@gmail.com>
2026-01-27 23:21:30 +0530

    change BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Arulsiva_Prortfolio">
       <App />
     </BrowserRouter>
   </StrictMode>
